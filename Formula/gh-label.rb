b6cff12631809095a849cc3e7513975abf015274
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-29 14:37:12 +0300

    Brew formula update for gh-label version v0.4.0

--- Formula/gh-label.rb
+++ Formula/gh-label.rb
@@ -2,16 +2,16 @@
 class GhLabel < Formula
   desc "A tool to help you manage GitHub issue labels"
   homepage "https://github.com/erdaltsksn/gh-label"
-  version "0.3.1"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/erdaltsksn/gh-label/releases/download/v0.3.1/gh-label_0.3.1_darwin_amd64.tar.gz"
-    sha256 "0874d2bb2d322568fd257798c48b1abb6e6be22a36d84e35bd9be62884586d9a"
+    url "https://github.com/erdaltsksn/gh-label/releases/download/v0.4.0/gh-label_0.4.0_darwin_amd64.tar.gz"
+    sha256 "ea1a5433ef5b661758601abba622064dc3be00a2c4bf812f5a43f7701c83ee5a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/erdaltsksn/gh-label/releases/download/v0.3.1/gh-label_0.3.1_linux_amd64.tar.gz"
-      sha256 "3b3a5edb9b1a1bfc6c8301af270be302c98acbb3a2ad0ae1a832db0c5a938b8b"
+      url "https://github.com/erdaltsksn/gh-label/releases/download/v0.4.0/gh-label_0.4.0_linux_amd64.tar.gz"
+      sha256 "6c818f08fe644cd56ce7aa10e5b02f6c2b704c2501f47c10ae10788343b1762f"
     end
   end
 
